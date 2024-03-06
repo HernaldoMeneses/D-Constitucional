2d6760611657421ac5c275045d48f1803fbc54eb
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-06 09:58:20 -0300

    Create 1.2.1-Conceito.py

---
+++ 1.2.1-Conceito.py
@@ -0,0 +1,27 @@
+'''
+    Todo Estado deve possuir uma Constituição
+        Limitação ao poder autoritário.
+        Prevalência dos direitos fundamentais.
+
+        H.Meneses
+            Iluminismo
+            afastamento do antigo regime
+'''
+
+Canotilho = '''
+    Constitucionalismo
+        Inglês
+        Americano
+        Francês
+            Movimentos Constitucionais
+                Teoria (Ideologia)
+                    Princípio do governo Limitado
+                    Indispensável à garantia dos direitos
+                    Estruturação da organização Político-social
+
+    **representa uma técnica específica de limitação do poder
+        com fins garantísticos.
+    **Tal qual a teoria da democracia ou do liberalismo
+        O constitucionalismo é um juízo de valor
+        Um teoria normativa da política
+'''
