4c36c74153cb7d0045d0709fd9705916e45dab32
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-27 11:08:40 -0300

    Update 1.2.1 - Conceito.py

--- 1.2.1-Conceito.py
+++ 1.2.1-Conceito.py
@@ -27,6 +27,16 @@ Canotilho = '''
 '''
 
 Kildare_Gon_Car = '''
+    Juridica - Sistema normativo, enfeixado na Constituição, e que se encontra acima dos detentores do poder;
+    Sociologica - Movimento social que dá sustentação à limitação do poder, inviabilizando que os governantes possam fazer prevalecer seus interesses e regras na condução do Estado.
 '''
 Andre_Ramos_Tav = '''
+    Sentido 1 - Movimento político-social com origens históricas bastante temotas que pretende, em especial, limitar o poder aritrário.
+    Sentido 2 - Imposição de que haja cartas constitucionais escritas (textualização).
+    Sentido 3 - Indicar prospósitos mais latentes e atuais da função e posição das constituições nas diversas sociedades.
+    Sentido 4 - Evolução histórico-constituciona de um determinado Estado.
+'''
+
+resumo_Geral = '''
+    Todo Estado deve possuir um Constituição, avançando-se no sentido de que os textos constitucionais contêm regras de Limitação ao poder autoritário e de prevalência dos direitos fundamentais.
 '''
